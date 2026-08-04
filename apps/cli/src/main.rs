9fafa395561f8a09c7176838d4907ffedcd752c7
Game K <hack.h@laposte.net>
2026-08-04 23:39:20 +0200

    feat: base cli

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -1,6 +1,24 @@
-use pakdo_core::add;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(name = "pakdo", version = "1.0", about = "Convertisseur de fichiers optimisé et 100 % local")]
+struct Args {
+    /// Fichier d'entrée
+    input: PathBuf,
+
+    /// Fichier de sortie
+    output: PathBuf,
+
+    /// Librairie à utiliser pour la convertion
+    #[arg(short, long)]
+    lib: Option<String>,
+}
 
 fn main() {
-    let resultat = add(2, 2);
-    println!("2 + 2 = {}", resultat);
+    let args = Args::parse();
+
+    println!("Fichier 1 : {:?}", args.input);
+    println!("Fichier 2 : {:?}", args.output);
+    println!("Lib : {:?}", args.lib);
 }
