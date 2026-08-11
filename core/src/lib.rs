5408006c1f134660af86a7bd381a8ac4649a4e1c
mathieuMarthy <mathieu.marthy@gmail.com>
2026-08-11 22:11:21 +0200

    feat: enhance output path validation and improve error handling

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -17,12 +17,27 @@ pub fn convert(
     let destination_path: PathBuf = match (output_path, target_extension) {
         (Some(output_path), None) => output_path.to_path_buf(),
         (None, Some(target_extension)) => input_path.with_extension(target_extension),
-        (Some(_), Some(_)) => return Err(Box::from(PakdoError::InvalidArguments(
-            "You must provide either an output path or a target extension, but not both.".into(),
-        ))),
-        (None, None) => return Err(Box::from(PakdoError::InvalidArguments(
-            "You must provide either an output path or a target extension.".into(),
-        ))),
+        (Some(output), Some(ext)) => {
+            if output.is_dir() || (!output.exists() && output.extension().is_none()) {
+                let output_file_name = input_path.with_extension(ext);
+                let file_name = output_file_name.file_name().ok_or_else(|| {
+                    PakdoError::InvalidArguments(
+                        "Cannot determine the file name from the input path.".into(),
+                    )
+                })?;
+                output.join(file_name)
+            } else {
+                return Err(Box::from(PakdoError::InvalidArguments(
+                    "When providing a target extension, the output path must be a directory."
+                        .into(),
+                )));
+            }
+        }
+        (None, None) => {
+            return Err(Box::from(PakdoError::InvalidArguments(
+                "You must provide either an output path or a target extension.".into(),
+            )));
+        }
     };
 
     convert_file(input_path, &destination_path, library)?;
@@ -30,21 +45,21 @@ pub fn convert(
     Ok(())
 }
 
-fn convert_file(input_path: &Path, output_path: &Path, library: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
+fn convert_file(
+    input_path: &Path,
+    output_path: &Path,
+    library: Option<&str>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let conversion_string = get_conversion_string(input_path, output_path)?;
 
     let conversion_function = match library {
-        Some(lib_name) => {
-            *LIBRARY.get(lib_name).ok_or_else(|| {
-                PakdoError::UnknownLibrary(lib_name.to_string())
-            })?
-        }
+        Some(lib_name) => *LIBRARY
+            .get(lib_name)
+            .ok_or_else(|| PakdoError::UnknownLibrary(lib_name.to_string()))?,
 
-        None => {
-            *ROUTES.get(conversion_string.as_str()).ok_or_else(|| {
-                PakdoError::ExtensionNotSupported(conversion_string)
-            })?
-        }
+        None => *ROUTES
+            .get(conversion_string.as_str())
+            .ok_or_else(|| PakdoError::ExtensionNotSupported(conversion_string))?,
     };
 
     conversion_function(input_path, output_path)
